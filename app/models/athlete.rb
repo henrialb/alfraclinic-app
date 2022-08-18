5a0b8400b7e1c9b7c830705ecfd5e9a95bb494df
Ines Louro <inespazlouro@gmail.com>
2022-08-18 19:41:29 +0100

    Revert "add archived to athletes"
    This reverts commit 1a07e14b6b0b02819570bdc2a6b03cd15ab330f4.

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -2,5 +2,4 @@ class Athlete < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   enum level: [:amateur, :professional]
-  enum archived: [:false, :true]
 end
--- db/migrate/20220818182444_add_archived_to_athletes.rb
+++
@@ -1,5 +0,0 @@
-class AddArchivedToAthletes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :athletes, :archived, :integer, default: 0
-  end
-end
