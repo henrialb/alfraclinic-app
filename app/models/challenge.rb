fbe116513cdb37ec0db8b464541fb62c28101c3f
Ines Louro <inespazlouro@gmail.com>
2022-08-21 09:36:07 +0100

    added challenges seeds

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,6 +2,7 @@ class Challenge < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   enum category: %i[strength velocity resistance]
+  enum result_unit: %i[un cal minutes cm]
 
   scope :archived, -> { where(archived: true) }
   scope :not_archived, -> { all - archived }
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,4 +18,33 @@ puts 'Creating athletes'
   )
 end
 
+puts '---------------'
+
+# Challenges
+puts 'Destroying challenges and associated records'
+Challenge.destroy_all
+
+puts 'Creating challenges'
+
+challenges = {
+  'light beams' => 0,
+  'cycling' => 1,
+  'treadmill running' => 2,
+  'flexibiliy' => 3
+}
+
+challenges.each do |challenge|
+  Challenge.create!(
+    name: challenge[0],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    result_unit: challenge[1],
+    category: rand(0..2)
+  )
+end
+
+puts '---------------'
 puts 'Done!'
