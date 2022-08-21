59238c12b0ecc5d319763b7eb03a93ce6768ac1d
Ines Louro <inespazlouro@gmail.com>
2022-08-21 09:50:23 +0100

    added challenges blueprint, routes and controller

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,9 +1,19 @@
 class Challenge < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
+  # Only allow deleting a challenge if it is archived
+  before_destroy :abort_destroy, unless: :archived?
+
   enum category: %i[strength velocity resistance]
   enum result_unit: %i[un cal minutes cm]
 
   scope :archived, -> { where(archived: true) }
   scope :not_archived, -> { all - archived }
+
+  private
+
+  def abort_destroy
+    errors.add :base, "an active challenge can't be deleted"
+    throw(:abort)
+  end
 end
